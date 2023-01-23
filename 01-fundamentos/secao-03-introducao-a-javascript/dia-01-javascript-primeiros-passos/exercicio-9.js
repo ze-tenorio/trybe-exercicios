e9bdb943c928880a31dda90eaa7b63d9b65ab20c
ze-tenorio <jlptenorio@gmail.com>
2023-01-23 22:49:12 +0000

    ex9 sobre if else e operadores > e && realizado

---
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-9.js
@@ -0,0 +1,11 @@
+const num1 = 17;
+const num2 = 37;
+const num3 = 45;
+
+if (num1 > num2 && num1 > num3) {
+    console.log(num1 + " é o maior número.")
+} else if (num2 > num1 && num2 > num3) {
+    console.log(num2 + " é o maior número.")
+} else {
+    console.log(num3 + " é o maior número.")
+}
