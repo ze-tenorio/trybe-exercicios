12b5bb3c2bad9d728e26456b1406a27e48a0347f
ze-tenorio <jlptenorio@gmail.com>
2023-01-23 22:42:09 +0000

    primeiro requisito do ex7 realizado: criadas operacoes de adicao, subtracao, divisao, multiplicacao e modulo

---
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-7.js
@@ -0,0 +1,14 @@
+const a = 24;
+const b = 8;
+
+let adicao = a + b;
+let subtracao = a - b;
+let multiplicacao = a * b;
+let divisao = a / b;
+let modulo = a % b;
+
+console.log(adicao);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(modulo);
