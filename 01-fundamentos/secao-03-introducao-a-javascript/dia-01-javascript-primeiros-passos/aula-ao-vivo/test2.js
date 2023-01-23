3f5daf469b321d2b58b53fa950616450a6d5ceeb
ze-tenorio <jlptenorio@gmail.com>
2023-01-23 21:05:43 +0000

    aula ao vivo finalizada e exercicios realizados

---
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/aula-ao-vivo/test2.js
@@ -0,0 +1,10 @@
+let num1 = 15;
+let num2 = 10;
+
+if (num1 === num2) {
+    console.log(num1 + num2);
+} else if (num1 !== num2) {
+    console.log(num1 * num2);
+} else if (num1 > num2) {
+    console.log(num1 / num2);
+}
