b37b3142362d5a695b72af7e6a2cc52ad362d461
ze-tenorio <jlptenorio@gmail.com>
2023-01-24 22:18:28 +0000

    ex6: adicionada estrutura para determinar quantos números impares existem no array

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-for/exercicio-6.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-for/exercicio-6.js
@@ -1,3 +1,4 @@
+//Some todos os valores contidos no array e imprima o resultado;
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 
@@ -5,16 +6,19 @@ for (let index = 0; index < numbers.length; index += 1) {
     soma = soma + numbers[index];
 }
 
+//Calcule e imprima a média aritmética dos valores contidos no array;
 let media = soma / numbers.length;
 
 console.log(media);
 
+//Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”;
 if (media > 20) {
     console.log("Valor maior que 20");
 } else {
     console.log("Valor menor que 20");
 }
 
+//Utilizando for, descubra qual o maior valor contido no array e imprima-o;
 let maiorNumero = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
@@ -24,3 +28,15 @@ for (let index = 0; index < numbers.length; index += 1) {
 }
 
 console.log(maiorNumero);
+
+//Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “nenhum valor ímpar encontrado”;
+
+let impares = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] % 2 !== 0) {
+        impares += 1;
+    }
+}
+
+console.log(impares);
