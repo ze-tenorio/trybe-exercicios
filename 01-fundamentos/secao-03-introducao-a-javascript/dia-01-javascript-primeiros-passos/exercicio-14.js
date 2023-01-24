a9a87e26b2b81b02cb2deb2d41168047aecd658d
ze-tenorio <jlptenorio@gmail.com>
2023-01-24 17:57:00 +0000

    segunda parte do ex14 realizada: if else para numeros impares

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-14.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-14.js
@@ -7,3 +7,13 @@ if (num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0) {
 } else {
     console.log(false)
 }
+
+const num4 = 12;
+const num5 = 24;
+const num6 = 8;
+
+if (num4 % 2 !== 0 || num5 % 2 !== 0 || num6 % 2 !== 0) {
+    console.log(true)
+} else {
+    console.log(false)
+}
