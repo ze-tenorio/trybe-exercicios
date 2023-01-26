fdcca977819b61d222f9596c375102fddd612bab
ze-tenorio <jlptenorio@gmail.com>
2023-01-26 19:06:26 +0000

    ex1 finalizado exibindo no console mensagem de quantas medalhas marta ganhou

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos-funcoes/exercicio-1.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos-funcoes/exercicio-1.js
@@ -13,3 +13,5 @@ console.log("A jogadora " + player.name + ' ' + player.lastName + ' tem ' + play
 player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
 
 console.log("A jogadora " + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por ' + player['bestInTheWorld'].length + ' vezes')
+
+console.log("A jogadora " + player.name + ' ' + player.lastName + ' possui ' + player.medals.golden + ' medalhas de ouro ' + 'e ' + player.medals.silver + ' medalhas de prata')
