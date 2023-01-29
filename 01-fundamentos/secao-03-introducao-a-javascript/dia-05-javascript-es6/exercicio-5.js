d9aa4f8327f9651c0e709d055883279cbe144b5c
ze-tenorio <jlptenorio@gmail.com>
2023-01-29 17:52:09 +0000

    ex5: regular function pattern alterado para arrow function para simplificar

---
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6/exercicio-5.js
@@ -0,0 +1,7 @@
+// function numeroAleatorio() {
+//     return Math.random()
+// }
+// console.log(numeroAleatorio());
+
+const numeroAleatorio = () => Math.round(Math.random() * 10);
+console.log(numeroAleatorio())
