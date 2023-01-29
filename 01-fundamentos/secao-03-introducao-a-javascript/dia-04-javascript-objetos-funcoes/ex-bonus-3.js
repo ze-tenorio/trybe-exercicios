62405163a2306f9183d02c5e7a9edaea9c70738a
ze-tenorio <jlptenorio@gmail.com>
2023-01-29 17:07:19 +0000

    ex-bonus-3 finalizado: criada estrutura que conta quantas frutas de cada tem no array basket

---
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos-funcoes/ex-bonus-3.js
@@ -0,0 +1,50 @@
+const basket = [
+    'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+    'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+    'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+    'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+    'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+    'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+    'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+    'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+    'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+    'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+    'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+    'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+    'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+    'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+    'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+    'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+    'Banana', 'Pera', 'Abacate', 'Uva',
+];
+
+//variavel que armazena um objeto com os tipos de frutas e quantidade
+let fruitsList = {};
+
+//loop que percorre o array basket
+for (let fruit of basket) {
+    //preenchendo o objeto fruitsList
+    fruitsList[fruit] = 0;
+}
+
+//percorrendo dnv o array basket
+for (let fruit of basket) {
+    for (let fruitType in fruitsList) {
+        if (fruit === fruitType) {
+            fruitsList[fruitType] += 1;
+        }
+    }
+}
+
+let summaries = [];
+
+//criando o output
+for (fruit in fruitsList) {
+    let message = `${fruitsList[fruit]} ${fruit}`;
+    if (fruitsList[fruit] > 1) message += 's';
+    summaries.push(message);
+}
+
+console.log(summaries)
+console.log(`Sua cesta possui: ${summaries.join(', ')}.`);
+
