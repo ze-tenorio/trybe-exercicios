d4f53cebf33d728c0603db189aad9cb233f42adc
ze-tenorio <jlptenorio@gmail.com>
2023-01-29 14:16:07 +0000

    ex-bonus-3 finalizado: criada estrutura que reproduz um triangulo retangulo invertido

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-logica-e-algoritmos/ex-bonus-3.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-logica-e-algoritmos/ex-bonus-3.js
@@ -1,35 +1,19 @@
-//Agora inverta o lado do triângulo. Por exemplo:
+n = 4;
 
-//preciso de uma variável que armazena o tamanho do triangulo retangulo invertido
-let n = 5;
+let asteriscos = 1;
 
-//preciso de uma variavel que armazene o que deve ser imprimido a cada linha
-let triangleLines = '';
-let asterisco = 1;
-let espacos = n;
-
-//vou ter que primeiro imprimir uma linha com n espaços 
-for (let contColuna = 1; contColuna < n; contColuna++) {
-    triangleLines = '';
-    for (let contLinha = 1; contLinha <= n - 1; contLinha++) {
-        triangleLines += 'a';
-        if (triangleLines.length === n - 1) {
-            triangleLines += '*';
+for (let linhas = 1; linhas <= n; linhas += 1) {
+    let spaces = n - 1;
+    let square = '';
+    for (let colunas = 1; colunas <= n; colunas += 1) {
+        if (spaces >= asteriscos) {
+            square += ' ';
+            spaces -= 1;
+        } else {
+            square += '*';
         }
-        
     }
-    console.log(triangleLines);
+    console.log(square)
+    asteriscos += 1;
 }
-
-
-
-console.log(triangleLines);
-
-//vou ter que  imprimir uma linha com n-1 espaços
-
-
-//e n-4 asterisco
-
-
-
-//depois vou ter que criar uma estrutura de loop que aumente o 1 no número de espaços e diminua o 4 no número de asteriscos
+ 
