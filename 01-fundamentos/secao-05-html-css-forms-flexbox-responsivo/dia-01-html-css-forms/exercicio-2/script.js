6f5b5e4a5fe1260f3b4532412f13f7639735d1e2
ze-tenorio <jlptenorio@gmail.com>
2023-02-08 21:43:19 +0000

    exercicio 2 finalizado: utilização do preventDefault

---
+++ 01-fundamentos/secao-05-html-css-forms-flexbox-responsivo/dia-01-html-css-forms/exercicio-2/script.js
@@ -0,0 +1,19 @@
+// SELETORES
+const INPUT_TEXT = document.querySelector("#input-text");
+const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
+const HREF_LINK = document.querySelector("#href");
+
+HREF_LINK.addEventListener('click', (event) => {
+    event.preventDefault();
+})
+
+INPUT_CHECKBOX.addEventListener('click', (event) => {
+    event.preventDefault();
+})
+
+INPUT_TEXT.addEventListener('keypress', (event) => {
+    const tecla = event.key === 'a'
+    if (!(tecla)) {
+        event.preventDefault();
+    }
+})
