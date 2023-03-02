943dbced145cebc9e2eabb854e33c064353aef7f
ze-tenorio <jlptenorio@gmail.com>
2023-03-02 19:07:47 -0300

    ex2 sobre HOF find finalizado

---
+++ 01-fundamentos/secao-07-higher-order-functions-js-es6/dia-01-intro-higher-order-functions/exercicio-2.js
@@ -0,0 +1,7 @@
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+// Adicione seu código aqui
+
+const findFizzBuzz = numbers.find(number => number % 3 === 0 && number % 5 === 0);
+
+console.log(findFizzBuzz);
