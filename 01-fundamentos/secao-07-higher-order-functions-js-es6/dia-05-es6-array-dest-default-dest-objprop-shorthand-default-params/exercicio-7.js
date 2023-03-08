28c6b762fdfce412ea60751bbfc2be1a625aa4aa
ze-tenorio <jlptenorio@gmail.com>
2023-03-08 18:44:55 -0300

    ex7 sobre array destructuring finalizado

---
+++ 01-fundamentos/secao-07-higher-order-functions-js-es6/dia-05-es6-array-dest-default-dest-objprop-shorthand-default-params/exercicio-7.js
@@ -0,0 +1,7 @@
+const myList = [5, 2, 3];
+
+// escreva swap abaixo
+
+const swap = ([firstNumber, secondNumber, thirdNumber]) => [thirdNumber, secondNumber, firstNumber];
+
+console.log(swap(myList));
