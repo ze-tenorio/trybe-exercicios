9b14bef4158e8b9c7c05206959034b28081006da
ze-tenorio <jlptenorio@gmail.com>
2023-03-29 08:25:52 -0300

    ex2 de fixação finalizado: primeiro componente react criado

---
+++ 02-front-end/secao-02-introducao-ao-react/dia-01-hello-world-react/exercicio-2/src/About.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class About extends React.Component {
+    render() {
+        return (
+            <section>
+                <h1>José Luiz Tenório</h1>
+                <p>Olá, bem vindo ao meu primeiro projeto React! Vqv rumo a formação de Full-Stack!</p>
+                <h2>Minhas habilidades:</h2>
+                <ul>
+                    <li>JavaScript</li>
+                    <li>CSS</li>
+                    <li>HTML</li>
+                    <li>Jest</li>
+                    <li>React</li>
+                </ul>
+            </section>
+        )
+    }
+}
+
+export default About;
---
+++ 02-front-end/secao-02-introducao-ao-react/dia-01-hello-world-react/exercicio-2/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import About from './About';
+
+class App extends React.Component {
+  render() {
+    return (
+      <main>
+        <About></About>
+      </main>
+    );
+  }
+}
+
+export default App;
