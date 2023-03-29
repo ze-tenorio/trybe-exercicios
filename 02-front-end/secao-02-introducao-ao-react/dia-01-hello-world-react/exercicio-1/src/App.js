e7711fb2c4668ef611c1a2ecae365f49fd66b40e
ze-tenorio <jlptenorio@gmail.com>
2023-03-29 08:09:19 -0300

    exercicio 1 finalizado: primeiros passos em reacts dados

---
+++ 02-front-end/secao-02-introducao-ao-react/dia-01-hello-world-react/exercicio-1/src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import HelloWorld from './HelloWorld';
+
+function App() {
+  return (
+    <div>
+      <HelloWorld/>
+      <HelloWorld/>
+      <HelloWorld/>
+    </div>
+  ) 
+}
+
+export default App;
