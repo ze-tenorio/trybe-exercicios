cf4190878462705ef51ede5f3e51d12b37efef79
ze-tenorio <jlptenorio@gmail.com>
2023-04-04 21:25:07 -0300

    regularizadas pastas da secao 3 de front end

---
+++ 02-front-end/secao-03-componentes-estado-eventos-forms-react/dia-01-componentes-estados-eventos/react-state/src/App.js
@@ -0,0 +1,62 @@
+import './App.css';
+import React from 'react';
+
+// atualizações
+
+class App extends React.Component {
+  state = {
+    btnOne: 0,
+    btnTwo: 0,
+    btnThree: 0
+  }
+
+  getButtonColor = (num) => num % 2 === 0 ? 'green' : 'white';
+  
+  handleClick = () => {
+    this.setState((prev) => ({
+      btnOne: prev.btnOne + 1
+    }))
+  }
+
+  handleClick2 = () => {
+    this.setState((prev) => ({
+      btnTwo: prev.btnTwo + 1
+    }))
+  }
+
+  handleClick3 = () => {
+    this.setState((prev) => ({
+      btnThree: prev.btnThree - 1
+    }), () => {
+      console.log(this.getButtonColor(this.state.btnThree))
+    })
+  }
+  
+  render () {
+    const { btnOne, btnTwo, btnThree } = this.state;
+    return (
+      <section>
+        <button 
+          onClick={this.handleClick}
+          style={ {backgroundColor: this.getButtonColor(btnOne)} }
+        >
+          {this.state.btnOne}
+        </button>
+        <button 
+          onClick={this.handleClick2}
+          style={{backgroundColor: this.getButtonColor(btnTwo)}} 
+        >
+          {this.state.btnTwo}
+        </button>
+        <button 
+          onClick={this.handleClick3}
+          style={{backgroundColor: this.getButtonColor(btnThree)}}
+        >
+          {this.state.btnThree}
+        </button>
+      </section>
+    )
+  }
+}
+
+export default App;
