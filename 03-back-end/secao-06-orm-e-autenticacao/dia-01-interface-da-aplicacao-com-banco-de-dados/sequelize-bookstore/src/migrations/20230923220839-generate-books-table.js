0860c48c2d193297d1c9349fc5f14d59922c8927
ze-tenorio <jlptenorio@gmail.com>
2023-09-23 19:29:48 -0300

    ex1 finalizado: config básica do sequelize e migration de criação da tabela books criada e migrada

---
+++ 03-back-end/secao-06-orm-e-autenticacao/dia-01-interface-da-aplicacao-com-banco-de-dados/sequelize-bookstore/src/migrations/20230923220839-generate-books-table.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('books', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      title: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      author: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      pageQuantity: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    })
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('books');
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
