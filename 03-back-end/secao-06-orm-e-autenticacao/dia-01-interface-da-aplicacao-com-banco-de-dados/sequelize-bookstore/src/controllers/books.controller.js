a9aa9362dbd11319dc95f8037efa9e674c722170
ze-tenorio <jlptenorio@gmail.com>
2023-09-23 19:58:34 -0300

    exs 7 e 8 finalizados: criado controller para getById e vinculado a rota GET /books/:id

--- 03-back-end/secao-06-orm-e-autenticacao/dia-01-interface-da-aplicacao-com-banco-de-dados/sequelize-bookstore/src/controllers/books.controller.js
+++ 03-back-end/secao-06-orm-e-autenticacao/dia-01-interface-da-aplicacao-com-banco-de-dados/sequelize-bookstore/src/controllers/books.controller.js
@@ -10,6 +10,21 @@ const getAll = async (_req, res) => {
   }
 };
 
+const getById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const book = await BookService.getById(id);
+    if (!book) {
+      return res.status(404).json({ message: 'Book not found' });
+    }
+    return res.status(200).json(book);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Ocorreu um erro' });
+  }
+};
+
 module.exports = {
   getAll,
+  getById,
 }
--- 03-back-end/secao-06-orm-e-autenticacao/dia-01-interface-da-aplicacao-com-banco-de-dados/sequelize-bookstore/src/index.js
+++ 03-back-end/secao-06-orm-e-autenticacao/dia-01-interface-da-aplicacao-com-banco-de-dados/sequelize-bookstore/src/index.js
@@ -8,3 +8,5 @@ app.use(express.json());
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
 
 app.get('/books', BookController.getAll);
+
+app.get('/books/:id', BookController.getById);
