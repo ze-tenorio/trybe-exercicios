abc3049f685d693e8871c66b0ed42a2d20773b26
ze-tenorio <jlptenorio@gmail.com>
2023-11-20 20:46:07 -0300

    exercício do dia finalizado: criada aplicação que conta requisições a uma API com suporte a dois bancos de dados (redis e sql)

---
+++ 03-back-end/secao-09-poo-e-solid/dia-05-solid-principios-l-i/src/index.ts
@@ -0,0 +1,79 @@
+// ./src/index.ts
+import { Connector, MySQLConnector, ReadOnlyRedisConnector, RedisConnector } from './Connectors';
+
+const token = 'ce42033d-9133-457a-a1a1-41ac0b63a333';
+
+const SQLconn = new MySQLConnector({
+  host: 'mysqldb',
+  port: 3306,
+  database: 'counter',
+  user: 'root',
+  password: 'example',
+});
+
+const redisConn = new RedisConnector({
+  host: 'redisdb',
+  port: 6379,
+});
+
+// const readOnlyRedisConn = new ReadOnlyRedisConnector({
+//   host: 'redisdb',
+//   port: 6379,
+// });
+
+const main = (connector: Connector) => {
+  connector.firstCount(token);
+
+  const logCount = async () => {
+    try {
+      const count = await connector.getCount(token);
+      console.log(count);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const doSomeIncrements = () => {
+    logCount();
+    connector.incrementCount(token);
+    connector.incrementCount(token);
+    connector.incrementCount(token);
+  };
+
+  const incrementsInterval = setInterval(doSomeIncrements, 500);
+
+  setTimeout(() => {
+    clearInterval(incrementsInterval);
+    logCount();
+    connector.clearCount(token);
+    connector.closeConnection();
+  }, 5500);
+};
+
+// const main = async (connector: ReadOnlyConnector) => {
+//   const count = await connector.getCount(token);
+//   try {
+//     console.log(count);
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
+
+main(SQLconn);
+main(redisConn);
+// main(readOnlyRedisConn);
+
+/*
+Saída:
+0
+3
+6
+9
+12
+15
+18
+21
+24
+27
+30
+*/
