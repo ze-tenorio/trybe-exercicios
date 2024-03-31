d7a065d485c69306294d5978b7a316b535fc7b27
ze-tenorio <jlptenorio@gmail.com>
2024-03-31 12:42:00 -0300

    exercícios do dia finalizados

---
+++ 05-eletiva-c#/secao-01-fundamentos-recursos-c#/dia-01-hello-world-c#-dotnet/HelloWorld/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Entrada de Dados
+int Age = int.Parse(Console.ReadLine());
+decimal Average = decimal.Parse(Console.ReadLine());
+bool Permission = bool.Parse(Console.ReadLine());
+string Name = Console.ReadLine();
+Console.WriteLine("Bem-vindo, " + Name + "! Você tem " + Average + " no banco e apenas " + Age + " anos.");
